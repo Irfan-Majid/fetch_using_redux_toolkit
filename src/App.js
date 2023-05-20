d791995234cf241fc4842cca7069bbcee067631f
Abdul Majid Irfan <78084618+Irfan-Majid@users.noreply.github.com>
2023-05-20 13:37:31 +0600

    Uploading existing file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { PostsView } from './features/posts/PostsView';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fetch using redux toolkit</h1>
+      <PostsView />
+    </div>
+  );
+}
+
+export default App;
